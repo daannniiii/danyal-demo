9b683c55797fbb440bd327dd81a4a38f82e3b3ba
Danyal Shoaib <danydon710@gmail.com>
2025-11-27 14:43:08 +0500

    wrote a code which checks if a number is even or not and print the even numbers till that number if the number is even

---
+++ checkeven-func.py
@@ -0,0 +1,13 @@
+def noteven (a):
+    if a % 2 == 0 :
+        return 1 
+    else:
+        return 0
+def tilleven (b):
+    for i in range(2,b+1,2):
+        print(i)
+n = int (input("Enter a number:"))
+if noteven(n):
+    tilleven(n)
+else:
+    print("The number entered is odd")
