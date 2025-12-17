06534fcc9d594d71ed355b415d3b3f6b23ca1b06
Danyal Shoaib <danydon710@gmail.com>
2025-12-17 23:19:55 +0500

    added a new file for testing different modules

--- Final-term-project/testing-module.py
+++ Final-term-project/testing-module.py
@@ -4,14 +4,15 @@ print("WELLCOME TO CARNIVEL CORNER".center(50))
 print("="*50,"\n LOGIN AS:\n 1.USER(U)\n 2.VENDER(V)\n 3.GUEST(G)\n 4.ADMIN(A)")
 # get user type and verify
 user_type = input()
-if user_type == "u" or "U":
+user_type = user_type.lower()
+
+if user_type == "u":
     print("user")
-elif user_type == "v" or "V":
+elif user_type == "v":
     print("vender")
-    
-elif user_type == "g" or "G":
+elif user_type == "g":
     print("guest")
-elif user_type == "a" or "A":
+elif user_type == "a":
     print("Admin")
 else:
     print("You entered an INVALID input")
---
+++ Final-term-project/testing.py
@@ -0,0 +1,5 @@
+words = ["Hello", "World", "Python"]
+sentence = " ".join(words)
+print(sentence)  # "Hello World Python"
+words = sentence.split()
+print(words)
